7677904690307a1795c74e91194a85cc829a6602
VikasMeshram2708 <meshramvikas744@gmail.com>
2023-01-09 11:52:34 +0530

    :sparkles: created sign in api and we're sending tokens when the user gets logged in every time it's new token.

--- API/auth.js
+++ API/auth.js
@@ -14,6 +14,9 @@ const User = db.get('users');
 // require bcryptjs for hashing the password
 const bcrypt = require('bcryptjs');
 
+// Require jwt for storingi the tokens
+const jwt = require('jsonwebtoken');
+
 // Route 1 : Create a new User Using POST "/api/auth/createUser"
 router.post('/createUser', async (req, res) => {
   try {
@@ -38,12 +41,13 @@ router.post('/createUser', async (req, res) => {
       const secPass = await bcrypt.hash(req.body.password, 10);
       user.password = secPass;
 
+      // keep track when the User was created
+      user.createdOn = new Date().toLocaleString();
       //   Insert to DB
-      const created = await User.insert(user);
+      await User.insert(user);
 
       return res.status(201).json({
         message:'User Registerd Successfully!',
-        user:created
       });
       // insert to db
     }
@@ -57,6 +61,54 @@ router.post('/createUser', async (req, res) => {
     });
   }
 });
+
+// Route 2 : User Login Using POST "/api/auth/userLogin"
+router.post('/userLogin', async (req, res) => {
+  try {
+    // Check if the use exist
+    const user = await User.findOne({
+      email:req.body.email,
+    });
+
+    if(user) {
+      // compare the password
+      const isValidKey = await bcrypt.compare(req.body.password, user.password);
+      
+      if(isValidKey) {
+
+        // sign with jwt token
+        const data = {
+          _id:user._id,
+          name:user.name,
+          email:user.email
+        };
+
+        const authToken = jwt.sign(data, process.env.JWT_SECRET);
+      
+        return res.status(201).json({
+          message:'User Logged In Successfully!',
+          token:authToken
+        });
+      }
+    }
+    return res.status(201).json({
+      message:'Try, to login with valid credentails!'
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message:'Some Internal Server Error',
+      error:error.message
+    });
+  }
+});
+
+
+
+
+
+
+
+
 
 
 module.exports =  router;
